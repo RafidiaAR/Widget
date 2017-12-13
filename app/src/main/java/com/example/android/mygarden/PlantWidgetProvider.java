fcdd105961b3763899a8f373211fcadc897cb498
RafidiaAR <rafidiaar007@gmail.com>
2017-12-13 14:04:29 -0800

    Exercise 4 : Adding More Features

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -5,23 +5,38 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
 
+import com.example.android.mygarden.provider.PlantContract;
 import com.example.android.mygarden.ui.MainActivity;
+import com.example.android.mygarden.ui.PlantDetailActivity;
 
 /**
  * Created by Rafid on 12/13/2017.
  */
 
 public class PlantWidgetProvider extends AppWidgetProvider {
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,int imgRes, int appWidgetId){
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,int imgRes, long plantId, boolean showWater, int appWidgetId){
+        Intent intent;
+        if(plantId == PlantContract.INVALID_PLANT_ID){
+            intent = new Intent(context, MainActivity.class);
+        }else{
+            Log.d(PlantWidgetProvider.class.getSimpleName(),"plantId=" + plantId);
+            intent = new Intent(context, PlantDetailActivity.class);
+            intent.putExtra(PlantDetailActivity.EXTRA_PLANT_ID, plantId);
+        }
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
         views.setImageViewResource(R.id.widget_plant_image, imgRes);
-        Intent intent = new Intent(context, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,intent,0);
+        views.setTextViewText(R.id.widget_plant_name, String.valueOf(plantId));
+        if (showWater) views.setViewVisibility(R.id.widget_water_button, View.VISIBLE);
+        else views.setViewVisibility(R.id.widget_water_button, View.INVISIBLE);
         views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
         Intent wateringIntent = new Intent(context, PlantWateringService.class);
-        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANTS);
+        wateringIntent.setAction(PlantWateringService.ACTION_WATER_PLANT);
+        wateringIntent.putExtra(PlantWateringService.EXTRA_PLANT_ID, plantId);
         PendingIntent wateringPendingIntent = PendingIntent.getService(
                 context,
                 0,
@@ -40,9 +55,9 @@ public class PlantWidgetProvider extends AppWidgetProvider {
         }
     }
     public static void updatePlantWidgets(Context context, AppWidgetManager appWidgetManager,
-                                          int imgRes, int[] appWidgetIds) {
+                                          int imgRes, long plantId, boolean showWater, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, imgRes, appWidgetId);
+            updateAppWidget(context, appWidgetManager, imgRes, plantId, showWater, appWidgetId);
         }
     }
 
--- app/src/main/java/com/example/android/mygarden/ui/PlantDetailActivity.java
+++ app/src/main/java/com/example/android/mygarden/ui/PlantDetailActivity.java
@@ -57,21 +57,7 @@ public class PlantDetailActivity extends AppCompatActivity
     }
 
     public void onWaterButtonClick(View view) {
-        Uri SINGLE_PLANT_URI = ContentUris.withAppendedId(
-                BASE_CONTENT_URI.buildUpon().appendPath(PATH_PLANTS).build(), mPlantId);
-        Cursor cursor = getContentResolver().query(SINGLE_PLANT_URI, null, null, null, null);
-        if (cursor == null || cursor.getCount() < 1) return;
-        cursor.moveToFirst();
-        long lastWatered = cursor.getLong(cursor.getColumnIndex(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME));
-        long timeNow = System.currentTimeMillis();
-        if ((timeNow - lastWatered) > PlantUtils.MAX_AGE_WITHOUT_WATER)
-            return;
-
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
-        getContentResolver().update(SINGLE_PLANT_URI, contentValues, null, null);
-        cursor.close();
-        PlantWateringService.startActionUpdatePlantWidgets(this);
+        PlantWateringService.startActionWaterPlant(this, mPlantId);
     }
 
     @Override
